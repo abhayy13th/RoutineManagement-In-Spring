fd61cef026d648c420c7837e356c3d00c78af373
Avaya Shrestha <68022642+abhayy13th@users.noreply.github.com>
2023-09-04 17:31:20 +0545

    First Commit

---
+++ src/test/java/com/backend_assignment/abhay/AbhayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend_assignment.abhay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbhayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
